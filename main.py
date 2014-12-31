856232a7584b3e161630cc2b78d060ba073acefa
agomez14 <Chivas@Armandos-MacBook-Pro.local>
2014-12-31 16:39:55 -0600

    Added url fetch request for TimeZone API

--- main.py
+++ main.py
@@ -14,28 +14,48 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2, jinja2, os, logging
+import webapp2, jinja2, os, logging, urllib2, urllib, json
 
-import settings
+from settings import *
+
+from google.appengine.api import urlfetch
 
 jinja_environment = jinja2.Environment(loader=
 	jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-YO_TOKEN = settings.YO_API_TOKEN
-URL = settings.YO_URL
-TIMEZONE_TOKEN = settings.TIMEZONE_API_TOKEN
+YO_TOKEN = YO_API_TOKEN
+TIMEZONE_KEY = TIMEZONE_API_KEY
+
+YO_URL = "http://dev.justyo.co"
+
+timestamp = "1420009914"
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         template_values = {}
         template = jinja_environment.get_template('main.html')
         self.response.out.write(template.render(template_values))
+
 class YoHandler(webapp2.RequestHandler):
 	def get(self):
-		template_values = {}
-		username = self.request.get('username')
-		location = self.request.get('location')
-		latitude = location.split(';')[0]
-		longitude = location.split(';')[1]
+		username = "CHIVAS604"
+		location = "42.360091;-71.094159"
+		url = "https://maps.googleapis.com/maps/api/timezone/json?"
+		temp = list(location)
+		index = location.index(';')
+		temp[index] = ","
+		location = "".join(temp)
+		parameters = [
+			("location",location),
+			("timestamp",timestamp)
+		]
+		form_data = urllib.urlencode(parameters)
+		response = urlfetch.fetch(url=url,
+			payload=form_data,
+			method=urlfetch.POST
+		).content
+		logging.info(response)
+		json_data = json.loads(response)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
